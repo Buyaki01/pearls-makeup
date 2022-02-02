184755975f927fd53a188bce4b1f105fe1293148
Buyaki <rittahbuyaki@gmail.com>
2022-02-02 23:48:11 +0300

    Add the logo icon

--- README.md
+++ README.md
@@ -58,7 +58,7 @@ It correctly bundles React in production mode and optimizes the build for the be
   - The build is minified and the filenames include the hashes.\
 Your app is ready to be deployed!
 
-## Authors
+## Author
 
 👤 **Ritta Sweta**
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,13 @@
+import { FaPaintBrush } from 'react-icons/fa';
+
 const Header = () => (
   <div className="header">
     <h1>
+      <FaPaintBrush
+        style={{
+          color: 'white', fontSize: '30px', marginTop: '2px', marginRight: '2px',
+        }}
+      />
       Pearls Makeup
     </h1>
   </div>
